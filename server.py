a46a230b4b8deec6a34f7a77c695cb3530bd148f
Amanda Gantugs <gantugsamanda@gmail.com>
2026-02-25 14:48:09 -0800

    adding necessary files

--- server.py
+++ server.py
@@ -1,3 +1,21 @@
 import socket
-HOST = '0.0.0'
+
+HOST = "127.0.0.1"
+PORT = 65432
+
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    print(f"Server is listening on {HOST}:{PORT}")
+    conn, addr = s.accept()
+    with conn:
+        print(f"Connected by {addr}")
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            print(f"Received data: {data.decode()}")
+            response = f"Echo: {data.decode()}"
+            conn.sendall(response.encode())
 
