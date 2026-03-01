ab1fcf1c09564752980b3eaddc516c4932edaf0e
Amanda Gantugs <gantugsamanda@gmail.com>
2026-02-28 18:22:17 -0800

    Update server.py

--- server.py
+++ server.py
@@ -1,43 +1,29 @@
 import socket
 import threading
+import os
 #HOST = "127.0.0.1"
-HOST = "server"
-PORT = 65432
-
-
-"""with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    s.bind((HOST, PORT))
-    s.listen()
-    print(f"Server is listening on {HOST}:{PORT}")
-    conn, addr = s.accept()
-    with conn:
-        print(f"Connected by {addr}")
-        while True:
-            data = conn.recv(1024)
-            if not data:
-                break
-            print(f"Received data: {data.decode()}")
-            response = f"Echo: {data.decode()}"
-            conn.sendall(response.encode())"""
+HOST = "0.0.0.0"
+PORT = int(os.getenv("PORT", 65432))
 
 def client_handling(conn, addr):
-     print(f"Connected by {addr}")
-     with conn:
+    print(f"Connected by {addr}")
+    with conn:
         while True:
             data = conn.recv(1024)
             if not data:
                 break
-            msg = data.decode(errors="replace")
-            print(f"Received from {addr}:{msg}")
+            msg = data.decode("utf-8", errors="replace")
+            print(f"Received from [{addr}]:{msg}")
             response = f"Echo: {msg}"
-            conn.sendall(response.encode())
-#print(f"Connection closed by {addr}")
+            conn.sendall(response.encode("utf-8"))
+    print(f"Connection closed by {addr}")
 
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+        s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         s.bind((HOST, PORT))
         s.listen()
-        print(f"Server is listening on {HOST}:{PORT}")
+        print(f"[*] Server is listening on {HOST}:{PORT}")
         while True:
             conn, addr = s.accept()
             t = threading.Thread(target=client_handling, args=(conn, addr), daemon=True)
